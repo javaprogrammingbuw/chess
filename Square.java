c3d352bb9da47a8ad0bc66c022082227ffb63ef5
Afshin Ghasemi <afshin.ghasemi@uni-weimar.de>
2019-05-02 23:54:01 +0200

    <final>

--- Square.java
+++ Square.java
@@ -4,6 +4,7 @@ public class Square extends JButton{
 
 	public String piece;               // here visibility is private but for our use i made it as public, donot forget to change later.
 
+
 	public Square(){
 		super();
 	}
